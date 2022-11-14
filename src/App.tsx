77dbf570122fe4d9730ee6f9663dd717861dceea
Yokota <ryok90@gmail.com>
2022-11-14 19:57:58 -0300

    feat: test

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import people from './people.json';
 
 function App() {
+  const [inDisplay, setInDisplay] = useState<number>();
+  const sorted = people.sort((a, b) => (a.last_name > b.last_name ? -1 : 1));
+  const filtered = sorted.filter((_, index) => !((index + 1) % 3));
+
+  useEffect(() => {
+    if (!filtered.length) return;
+    const selected = inDisplay ?? 0;
+    if (selected > filtered.length) return;
+    setInDisplay(selected);
+    setTimeout(() => {
+      setInDisplay(selected + 1);
+    }, filtered[selected]?.timeout * 1000);
+  }, [inDisplay, filtered]);
+
+  const name =
+    inDisplay !== undefined && filtered[inDisplay]
+      ? `${filtered[inDisplay].name} ${filtered[inDisplay].last_name}`
+      : '';
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{name}</p>
       </header>
     </div>
   );
